3feab2aa2988c636b4d0e11f527ec95c66bddb42
danielf9403 <103592213+danielf9403@users.noreply.github.com>
2023-02-21 22:50:59 -0500

    first commit

---
+++ clock.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+import time
+
+def clock():
+    current=time.strftime("%H:%M:%S")
+    label1['text']=current
+    root.after(1000,clock)
+    
+    
+root=tk.Tk()
+root.title("Digital Clock")
+label1=tk.Label(root,font='article 80', bg='black',fg='yellow')
+label1.grid(row=0,column=0)
+clock()
+
+
+root.mainloop()
